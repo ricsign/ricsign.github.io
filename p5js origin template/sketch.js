c3bb2ba1040dde2a78f672c9a6f7ae14d5e2482c
ricsign <ricsign123@gmail.com>
2020-09-23 10:27:12 -0600

    setting up a repo

---
+++ p5js origin template/sketch.js
@@ -0,0 +1,16 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  ellipse(100, 100, 150, 100);
+}
