e8a353121488602c0afff4b8960a1a169596ee42
ricsign <ricsign123@gmail.com>
2021-11-21 11:32:10 -0600

    major change
    1. index page created
    2. improved memory game script

--- memory_game/script.js
+++ memory_game/script.js
@@ -1,4 +1,4 @@
-let numCards = 20;
+let numCards = 8;
 let cardList = [];
 
 class Card {
@@ -29,11 +29,14 @@ let handleClick = (id) => {
     let neighborCard = idNum % 2 === 0 ? $("#card" + (idNum + 1)) : $("#card" + (idNum - 1));
     if (childText.hasClass("invisible")) {
         childText.removeClass("invisible");
+    } else{
+        return;
     }
 
     if (!(neighborCard.children().eq(0).hasClass("invisible"))) {
         if (!($(id).hasClass("discovered"))) $(id).addClass("discovered");
         if (!(neighborCard.hasClass("discovered"))) neighborCard.addClass("discovered");
+        $("#g-score").html(parseInt($("#g-score").html())+10);
     } else {
         // count how many invisible cards in total
         // when visible card > 1, all cards should be invisible
@@ -56,9 +59,25 @@ let handleClick = (id) => {
             }
         }
     }
+
+    $("#click").html(parseInt($("#click").html())+1);
+    $("#n-score").html(parseInt($("#g-score").html())-parseInt($("#click").html()));
+
+    // winning
+    if(parseInt($("#g-score").html()) === 5*numCards){
+        setTimeout(function(){
+            alert("Congratulations, you win! Your net score is "+parseInt($("#n-score").html()));
+            location.reload();
+        },1000);   
+    }
 }
 
 let setupGame = () => {
+    let numPairs = parseInt(prompt("Number of pairs:"));
+    while(isNaN(numPairs) || numPairs <= 0){
+        numPairs = parseInt(prompt("Number of pairs:"));
+    }
+    numCards = numPairs*2;
     for (let i = 0; i < numCards; ++i) {
         cardList.push(new Card(i, Math.floor(i / 2)));
     }
